c148ba1aef0bb8306609c4383b880756cbc9b9b2
Ethan Niser <whatplan00@gmail.com>
2023-07-16 17:45:47 -0500

    add htmx

--- index.tsx
+++ index.tsx
@@ -8,11 +8,14 @@ const app = new Elysia()
     html(
       <BaseHtml>
         <body>
-          <h1>Hello World</h1>
+          <button hx-post="/clicked" hx-swap="outerHTML">
+            Click Me
+          </button>
         </body>
       </BaseHtml>
     )
   )
+  .post("/clicked", () => <div>I'm from the server!</div>)
   .listen(3000);
 
 console.log(
@@ -27,6 +30,7 @@ const BaseHtml = ({ children }: elements.Children) => `
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>THE BETH STACK</title>
+  <script src="https://unpkg.com/htmx.org@1.9.3"></script>
 </head>
 
 ${children}

cb0ad42e2534f8515a20ead7463a837e42710626
Ethan Niser <whatplan00@gmail.com>
2023-07-16 17:35:22 -0500

    server base html

--- index.ts
+++ index.ts
@@ -1,7 +1,23 @@
 import { Elysia } from "elysia";
+import { html } from "@elysiajs/html";
 
-const app = new Elysia().get("/", () => "Hello World!").listen(3000);
+const app = new Elysia()
+  .use(html())
+  .get("/", ({ html }) => html(baseHtml))
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
 );
+
+const baseHtml = `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>THE BETH STACK</title>
+</head>
+<body>I'm a html document</body>
+`;

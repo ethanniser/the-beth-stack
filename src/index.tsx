bb49234c183f4a792455c2a9716f05f792c4e414
Abraham Anak Agung <abraham.agel@gmail.com>
2023-09-14 14:17:04 +0700

    add twind shim

--- src/index.tsx
+++ src/index.tsx
@@ -85,6 +85,7 @@ const BaseHtml = ({ children }: elements.Children) => `
   <title>THE BETH STACK</title>
   <script src="https://unpkg.com/htmx.org@1.9.3"></script>
   <script src="https://unpkg.com/hyperscript.org@0.9.9"></script>
+  <script type="module" src="https://cdn.skypack.dev/twind/shim"></script>
 </head>
 
 ${children}

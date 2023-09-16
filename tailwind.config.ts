4adeb86140b96e40e5230aa45a4850f9996a6d25
FleetAdmiralJakob <jakob.roessner@outlook.de>
2023-09-16 16:44:33 +0200

    Made the Tailwind Config typesafe

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{html,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+} satisfies Config;

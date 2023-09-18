351a9a01a530966b3760da8e7b130f3f174f0cd0
Ethan Niser <whatplan00@gmail.com>
2023-09-17 20:15:06 -0500

    cli build thing

---
+++ src/cli/build.ts
@@ -0,0 +1,13 @@
+const logs = await Bun.build({
+  entrypoints: ["./src/cli/index.ts"],
+  outdir: "./dist/cli",
+  target: "bun",
+  // splitting: true,
+  // minify: true,
+  external: ["elysia"],
+});
+
+if (!logs.success) {
+  console.log(logs.logs);
+  process.exit(1);
+}

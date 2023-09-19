f131a79c3a543c97d787d947069ef625e25b8fa1
Ethan Niser <whatplan00@gmail.com>
2023-09-19 18:53:57 -0500

    small render changes

--- packages/beth-stack/src/cache/render.ts
+++ packages/beth-stack/src/cache/render.ts
@@ -1,49 +1,35 @@
 import { Children } from "../jsx";
 import { BETH_GLOBAL_RENDER_CACHE } from "../shared/global";
 
-type Args<T> = T extends (...args: infer U) => any ? U : never;
+export function cache<T extends (...args: any[]) => any>(fn: T): T {
+  return ((...args: unknown[]) => {
+    const cachedResults = BETH_GLOBAL_RENDER_CACHE.dedupeCache.get(fn);
 
-function defaultCompare<T extends Array<any>>(arr1: T, arr2: T): boolean {
-  if (arr1.length !== arr2.length) return false;
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (!Object.is(arr1[i], arr2[i])) {
-      return false;
-    }
-  }
-  return true;
-}
-
-export function cache<T extends (...args: any[]) => any>(
-  fn: T,
-  compareFn: (oldArgs: Args<T>, newArgs: Args<T>) => boolean = defaultCompare,
-): T {
-  return ((...args: Args<T>) => {
-    console.log("cache called");
-    const cached = BETH_GLOBAL_RENDER_CACHE.dedupeCache.get(fn) || new Map();
-
-    BETH_GLOBAL_RENDER_CACHE.dedupeCache.set(fn, cached);
-
-    for (let [keyArgs, cachedValue] of cached.entries()) {
-      if (compareFn(args, keyArgs)) {
-        if (cachedValue.type === "error") {
-          console.log("throwing cached error");
-          throw cachedValue.error;
-        } else {
-          console.log("returning cached value");
-          return cachedValue.value;
+    if (cachedResults) {
+      for (let [keyArgs, cachedValue] of cachedResults.entries()) {
+        if (Bun.deepEquals(args, keyArgs, true)) {
+          if (cachedValue.type === "error") {
+            console.log("throwing cached error");
+            throw cachedValue.error;
+          } else {
+            console.log("returning cached value");
+            return cachedValue.value;
+          }
         }
       }
-    }
+    } else {
+      const newCache = new Map();
 
-    console.log("returning new value");
-    try {
-      const functionResult = fn(...args);
-      cached.set(args, { type: "result", value: functionResult });
-      return functionResult;
-    } catch (error) {
-      cached.set(args, { type: "error", error });
-      throw error;
+      BETH_GLOBAL_RENDER_CACHE.dedupeCache.set(fn, newCache);
+
+      try {
+        const functionResult = fn(...args);
+        newCache.set(args, { type: "result", value: functionResult });
+        return functionResult;
+      } catch (error) {
+        newCache.set(args, { type: "error", error });
+        throw error;
+      }
     }
   }) as T;
 }

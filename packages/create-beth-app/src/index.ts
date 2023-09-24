cdc14761eb929c3f9b784b140cb2b691b43accbc
Ethan Niser <whatplan00@gmail.com>
2023-09-24 11:33:49 -0500

    cli fix

--- packages/create-beth-app/src/index.ts
+++ packages/create-beth-app/src/index.ts
@@ -1,9 +1,36 @@
 #!/usr/bin/env bun
-Bun.spawn(["git", "clone", "https://github.com/ethanniser/beth-big.git"]);
 
-console.log(
-  "this cli is exetrely new and barebones, contributions are welcome",
-);
-console.log("https://github.com/ethanniser/the-beth-stack");
+await new Promise((res, rej) => {
+  Bun.spawn(
+    ["git", "clone", "https://github.com/ethanniser/beth-big.git", "."],
+    {
+      onExit(subprocess, exitCode, signalCode, error) {
+        if (exitCode === 0) {
+          res(null);
+        } else {
+          rej(error);
+        }
+      },
+    },
+  );
+});
+
+await new Promise((res, rej) => {
+  Bun.spawn(["bun", "install"], {
+    onExit(subprocess, exitCode, signalCode, error) {
+      if (exitCode === 0) {
+        res(null);
+      } else {
+        rej(error);
+      }
+    },
+  });
+});
 
 console.log("\n\n");
+console.log(
+  "this cli is extremely new and barebones, contributions are welcome",
+);
+console.log("https://github.com/ethanniser/the-beth-stack");
+console.log("looking for help? open an issue or ask in the discord");
+console.log("Ethan's Discord: https://discord.gg/Z3yUtMfkwa");
